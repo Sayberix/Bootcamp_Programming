1f6c827ebc58ec9bd857e7db529c7f3b521c8003
Maxim Konev <sayberix.ru@yandex.ru>
2022-08-02 23:15:30 +0700

    алгоритм Евклида

---
+++ алгоритм Евклида/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace алгоритм_Евклида
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // алгоритм Евклида
+            int n = 140;
+            int m = 175;
+            int count = n * m;
+            while(n != m)
+            {
+                if (n > m)
+                    n -= m;
+                else
+                    m -= n;
+                Console.WriteLine(n);
+                Console.WriteLine(m);
+                Console.WriteLine();
+            }
+            // НОК
+            Console.WriteLine("НОК: " + count / m);
+        }
+    }
+}
