e164b808fb71df7a93ffe8db7e61c95ba3fec5f9
Maxim Konev <sayberix.ru@yandex.ru>
2022-08-03 20:24:22 +0700

    2-ая версия

--- алгоритм Евклида/Program.cs
+++ алгоритм Евклида/Program.cs
@@ -13,7 +13,7 @@ namespace алгоритм_Евклида
             {
                 for (int j = 1; j <= n; j++)
                 {
-                    Console.WriteLine($"{i} * {j} = {i * j}");
+                    Console.Write($"{i * j} \t");
                 }
                 Console.Write("\n");
             }
