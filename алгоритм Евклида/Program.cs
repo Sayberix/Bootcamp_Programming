17bbf13ea74cf0037f03577c18954f3e1c5bf3b5
Maxim Konev <sayberix.ru@yandex.ru>
2022-08-03 20:06:07 +0700

    начало работы над таблицей умножения

--- алгоритм Евклида/Program.cs
+++ алгоритм Евклида/Program.cs
@@ -6,22 +6,8 @@ namespace алгоритм_Евклида
     {
         static void Main(string[] args)
         {
-            // алгоритм Евклида
-            int n = 140;
-            int m = 175;
-            int count = n * m;
-            while(n != m)
-            {
-                if (n > m)
-                    n -= m;
-                else
-                    m -= n;
-                Console.WriteLine(n);
-                Console.WriteLine(m);
-                Console.WriteLine();
-            }
-            // НОК
-            Console.WriteLine("НОК: " + count / m);
+            // таблица умножения
+
         }
     }
 }
