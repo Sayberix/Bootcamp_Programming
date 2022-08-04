7d29d1f261e76e4022252e535e59b54f8780015d
Maxim Konev <sayberix.ru@yandex.ru>
2022-08-04 23:33:27 +0700

    сортировка выбором

--- алгоритм Евклида/Program.cs
+++ алгоритм Евклида/Program.cs
@@ -6,17 +6,57 @@ namespace алгоритм_Евклида
     {
         static void Main(string[] args)
         {
-            // таблица умножения
-
-            int n = 5;
-            for (int i = 1; i <= n; i++)
+            /*
+            [6, 15, 2, 9, -3]
+            MIN = 6
+            6 < 15
+            6 > 2
+            MIN = 2
+            2 < 9
+            2 > -3
+            MIN = -3
+            [-3, 6, 15, 2, 9]
+            [6, 15, 2, 9]
+            MIN = 6
+            6 < 15
+            6 > 2
+            MIN = 2
+            2 < 9
+            [-3, 2, 6, 15, 9]
+            MIN = 6
+            6 < 15
+            6 < 9
+            [-3, 2, 6, 15, 9]
+            MIN = 15
+            15 > 9
+            [-3, 2, 6, 9, 15]
+            */
+            Console.WriteLine("Введите кол-во элементов массива:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            // Заполнение массива
+            int[] array = new int[n];
+            for (int i = 0; i < n; i++)
             {
-                for (int j = 1; j <= n; j++)
-                {
-                    Console.Write($"{i * j} \t");
-                }
-                Console.Write("\n");
+                Console.Write("Введите число: ");
+                array[i] = Convert.ToInt32(Console.ReadLine());
             }
+            Console.WriteLine();
+            Console.WriteLine("Начальный массив: [" + string.Join(", ", array) + "]");
+            // Cортировка
+            for (int i = 0; i < n - 1; i++)
+            {
+                int MinIndex = i;
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (array[j] < array[MinIndex])
+                        MinIndex = j;
+                }
+                int temp;
+                temp = array[MinIndex];
+                array[MinIndex] = array[i];
+                array[i] = temp;
+            }
+            Console.WriteLine("Конечный массив: [" + string.Join(", ", array) + "]");
         }
     }
 }
