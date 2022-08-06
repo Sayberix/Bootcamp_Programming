17e44b5e5b2fcc2d4486986cc370219e19c38c06
Maxim Konev <sayberix.ru@yandex.ru>
2022-08-06 12:50:11 +0700

    блок ввода и заполнение массива случайными числами

--- алгоритм Евклида/Program.cs
+++ алгоритм Евклида/Program.cs
@@ -11,7 +11,17 @@ namespace алгоритм_Евклида
             Необходимо сдвинуть всю последовательность (сдвиг - циклический) на |K| элементов вправо, если K – положительное 
             и влево, если отрицательное.
             */
-
+            Console.WriteLine("Введите кол-во элементов массива:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите k (отрицательное или положительное число):");
+            int k = Convert.ToInt32(Console.ReadLine());
+            // Заполнение массива
+            int[] array = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = new Random().Next(10);
+            }
+            
         }
     }
 }
